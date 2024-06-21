404bbee2fc4224b4e1d6e7d9156350e09b36d1da
Mamta Paswan <mamtapaswan2021@gmail.com>
2024-06-21 18:17:38 +0530

    Single Page completed

--- single.php
+++ single.php
@@ -17,40 +17,45 @@ get_header(); ?>
 
 <div class="blog-header">
   <div class="container">
-    <div class="col-lg-9">
-      <p id="breadcrumbs">
-        <span>
-          <span>
-            <a href="#">Home</a>
-          </span>
-          <span>
-            <a href="#">Blog</a>
-          </span>
-          <span>
-            <a href="#">Banking</a>
-          </span>
-        </span>
-      </p>
-      <h1>Popular Best Google Doodle Games — Dive Into the Mini Arcade</h1>
-      <div class="blog-detail flex-between-wrap">
-        <span class="mg-timeline">
-          <a href="#" class="author-img">
-            <img src="https://getassist.net/wp-content/uploads/2023/12/janvi-panthri-newsize.webp" alt="" class="img-fluid">
-          </a>
-          <span> By <a href="#">Janvi Panthri</a></span>
-        </span>
-        <span class="mg-timeline">
-          <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M15.833 3.333H4.167C3.247 3.333 2.5 4.08 2.5 5v11.667c0 .92.746 1.666 1.667 1.666h11.666c.92 0 1.667-.746 1.667-1.666V5c0-.92-.746-1.667-1.667-1.667m-2.5-1.666V5M6.667 1.667V5M2.5 8.333h15" stroke="#009A77" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-          </svg>
 
-          Updated on : 9th May 2024
-        </span>
-        <span class="mg-timeline">
-          6 mins read
-        </span>
-      </div>
-    </div>
+    <?php if (have_posts()) : while (have_posts()) :  the_post(); ?>
+
+        <div class="col-lg-9">
+          <p id="breadcrumbs">
+            <span>
+              <span>
+                <a href="#">Home</a>
+              </span>
+              <span>
+                <a href="#">Blog</a>
+              </span>
+              <span>
+                <a href="#">Banking</a>
+              </span>
+            </span>
+          </p>
+          <h1><?php echo get_the_title(); ?></h1>
+          <div class="blog-detail flex-between-wrap">
+            <span class="mg-timeline">
+              <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>" class="author-img">
+                <?php echo get_avatar(get_the_author_meta('ID'), 45); ?>
+              </a>
+              <span> By <a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>"><?php echo get_the_author(); ?></a></span>
+            </span>
+            <span class="mg-timeline">
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M15.833 3.333H4.167C3.247 3.333 2.5 4.08 2.5 5v11.667c0 .92.746 1.666 1.667 1.666h11.666c.92 0 1.667-.746 1.667-1.666V5c0-.92-.746-1.667-1.667-1.667m-2.5-1.666V5M6.667 1.667V5M2.5 8.333h15" stroke="#009A77" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+              </svg>
+
+              Updated on : <?php echo get_the_date('F j,Y') ?>
+            </span>
+            <span class="mg-timeline">
+              6 mins read
+            </span>
+          </div>
+        </div>
+
+
   </div>
 </div>
 
@@ -58,7 +63,7 @@ get_header(); ?>
   <div class="row row-gap mg-con">
     <div class="col-lg-9">
       <div class="blog-content">
-        <?php $blog_content = apply_filters('the_content', get_the_content()) ?>
+        <?php echo $blog_content = apply_filters('the_content', get_the_content()) ?>
       </div>
     </div>
     <div class="col-lg-3">
@@ -66,33 +71,53 @@ get_header(); ?>
     </div>
   </div>
 
-  <div class="mg-con-related-post">
-    <div class="sec-title">
-      <div class="h2">Related posts</div>
-    </div>
-    <div class="row row-gap">
-      <div class="col-lg-4 col-md-6">
-        <div class="mg-card">
-          <div class="img-placeholder">
-            <a href="#">
-              <img src="https://ityug247.com/wp-content/uploads/2023/09/social-media-and-e-commerce.webp" alt="" class="img-fluid">
-            </a>
-          </div>
-          <div class="mg-card-content">
-            <a href="#" class="cat-btn">blog</a>
-            <a href="#" class="blog-title">Here are 15 Sure-Shot Ways to Fix “Netflix Error Code</a>
-            <p>Lorem ipsum dolor, sit amet sit amet consectetur adipisicing elit.</p>
-            <a href="#" class="mg-btn-txt">
-              Read More
-              <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1 5.25a.75.75 0 0 0 0 1.5zm12.53 1.28a.75.75 0 0 0 0-1.06L8.757.697a.75.75 0 1 0-1.06 1.06L11.939 6l-4.242 4.243a.75.75 0 0 0 1.06 1.06zM1 6.75h12v-1.5H1z" fill="#000"></path>
-              </svg>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+<?php
+      endwhile;
+    endif;
+?>
+
+<div class="mg-con-related-post">
+  <div class="sec-title">
+    <div class="h2">Related posts</div>
   </div>
+
+  <?php
+  // Get the categories of the current post
+  $categories = get_the_category();
+  // print_r($categories)
+
+  if ($categories) {
+    $cat_ids = array();
+    foreach ($categories as $cat) {
+      $cat_ids = $cat->term_id;
+    }
+  }
+
+  // Set up arguments for the related posts query
+  $args = array(
+    'post_type' => 'post',
+    'post_status' => 'publish',
+    'category__in' => $cat_ids,
+    'post__not_in' => array(get_the_ID()),
+    'posts_per_page' => 6, // Change the number of related posts to display here
+  );
+
+  $related_posts = new WP_Query($args);
+  // Output the related posts
+  ?>
+  <div class="row row-gap">
+    <?php if ($related_posts->have_posts()) :
+      while ($related_posts->have_posts()) :
+        $related_posts->the_post(); ?>
+        <div class="col-lg-4 col-md-6">
+          <?php get_template_part('template-parts/content', 'card'); ?>
+        </div>
+    <?php endwhile;
+    endif;
+    wp_reset_postdata();
+    ?>
+  </div>
+</div>
 </div>
 
 <?php get_footer(); ?>
